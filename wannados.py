6f383dfe900671f215fdd58399dad88356c30655
FxzLuv <84973927+FxzLuv@users.noreply.github.com>
2021-05-28 15:58:35 +0300

    Encoding fix.

--- wannados.py
+++ wannados.py
@@ -226,7 +226,7 @@ else:
 t = Fore.MAGENTA + time.strftime("[%H:%M:%S]", time.localtime())
 
 
-what = '0' # лень было под True
+what = '0' # Р»РµРЅСЊ Р±С‹Р»Рѕ РїРѕРґ True
 conn = socket.socket()
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -291,14 +291,14 @@ def ban():
     print("-------------------------------------------------\n")
 
 def man():
-    print(Fore.YELLOW +  " [1] Start attack | Начать атаку")
+    print(Fore.YELLOW +  " [1] Start attack | РќР°С‡Р°С‚СЊ Р°С‚Р°РєСѓ")
     print(Fore.WHITE + "")
-    print(" [2] Узнать айпи домена, cloudflare cracker domens ip")
+    print(" [2] РЈР·РЅР°С‚СЊ Р°Р№РїРё РґРѕРјРµРЅР°, cloudflare cracker domens ip")
     print(Fore.RED + "")                             
-    print(" [3] Сканировать порты | Scan open ports")
+    print(" [3] РЎРєР°РЅРёСЂРѕРІР°С‚СЊ РїРѕСЂС‚С‹ | Scan open ports")
     print(Fore.BLUE + " ")                                                    
-    print(" [4] Чат с DoS-ерами прямо не выходя из Termux, вы можете действовать слаженно")
-    print("     чтобы повалить сильный сервер |")
+    print(" [4] Р§Р°С‚ СЃ DoS-РµСЂР°РјРё РїСЂСЏРјРѕ РЅРµ РІС‹С…РѕРґСЏ РёР· Termux, РІС‹ РјРѕР¶РµС‚Рµ РґРµР№СЃС‚РІРѕРІР°С‚СЊ СЃР»Р°Р¶РµРЅРЅРѕ")
+    print("     С‡С‚РѕР±С‹ РїРѕРІР°Р»РёС‚СЊ СЃРёР»СЊРЅС‹Р№ СЃРµСЂРІРµСЂ |")
     print("   ")
     print("    |  Chat with other attackers without leaving Termux, you can attack together,")
     print("     to bring down a strong server.")
@@ -307,7 +307,7 @@ def man():
 while True:
     ban()
     man()
-    a = input(" Write parametr | Введите параметр : ")
+    a = input(" Write parametr | Р’РІРµРґРёС‚Рµ РїР°СЂР°РјРµС‚СЂ : ")
 
     if a == "1":
 
@@ -317,7 +317,7 @@ while True:
         print(' [3] DOS Attack OVH\n')
         print(' [4] Xerxes Dos attack\n\n')
 
-        wdos = input(Fore.BLUE + ' Choose script | Выберите скрипт: ')
+        wdos = input(Fore.BLUE + ' Choose script | Р’С‹Р±РµСЂРёС‚Рµ СЃРєСЂРёРїС‚: ')
         ban()
 
         if wdos == '1':
@@ -364,9 +364,9 @@ while True:
 
 
         elif wdos == '2':
-            ip = input("Введите ip адрес жертвы | Type ip adress of victim: ")
-            port = input("Введите порт | Port: ")
-            strong = input("Кол-во потоков (сила) | Current/strong of attack \n Желательно от 150 до 700 | Preferably in range of 150 700: ")
+            ip = input("Р’РІРµРґРёС‚Рµ ip Р°РґСЂРµСЃ Р¶РµСЂС‚РІС‹ | Type ip adress of victim: ")
+            port = input("Р’РІРµРґРёС‚Рµ РїРѕСЂС‚ | Port: ")
+            strong = input("РљРѕР»-РІРѕ РїРѕС‚РѕРєРѕРІ (СЃРёР»Р°) | Current/strong of attack \n Р–РµР»Р°С‚РµР»СЊРЅРѕ РѕС‚ 150 РґРѕ 700 | Preferably in range of 150 700: ")
             list_files = subprocess.run(["python", "sub/2/hammer.py" ,"-s"  , ip , "-p" , port , "-t" , strong])
 
         elif wdos == '3':
@@ -388,28 +388,28 @@ while True:
         list_files = subprocess.run(["python", "core/cloud.py"])
 
     elif a == "3":
-        ip1 = input("Введите ip адрес для сканирования | Type ip for scanning ports: ")
-        port1 = input("Введите порты | Write ports \n Пример | Example 21-80,443,7777,8080: ")
+        ip1 = input("Р’РІРµРґРёС‚Рµ ip Р°РґСЂРµСЃ РґР»СЏ СЃРєР°РЅРёСЂРѕРІР°РЅРёСЏ | Type ip for scanning ports: ")
+        port1 = input("Р’РІРµРґРёС‚Рµ РїРѕСЂС‚С‹ | Write ports \n РџСЂРёРјРµСЂ | Example 21-80,443,7777,8080: ")
         list_files = subprocess.run(["nmap" , "-sV" , ip1 , "-p" , port1])
     elif a == "4":
         ban()
         print(Fore.WHITE + " ")
-        print("  В открывшемся окне, после Irssi, пишем такие команды: ")
+        print("  Р’ РѕС‚РєСЂС‹РІС€РµРјСЃСЏ РѕРєРЅРµ, РїРѕСЃР»Рµ Irssi, РїРёС€РµРј С‚Р°РєРёРµ РєРѕРјР°РЅРґС‹: ")
         print(Fore.RED + " ")
         print("     /connect chat.freenode.com") 
         print("   ")
         print("     /join doser")
         print(Fore.WHITE + " ")
-        print("Все! Вы в чате!!!")
+        print("Р’СЃРµ! Р’С‹ РІ С‡Р°С‚Рµ!!!")
         print(" ")
         print(Fore.YELLOW + " ")
 
         print(" [1] Irssi")
         print(" ")
-        print(" [2] Установить модуль Irssi")
+        print(" [2] РЈСЃС‚Р°РЅРѕРІРёС‚СЊ РјРѕРґСѓР»СЊ Irssi")
         print(" ")
         print(Fore.GREEN + " ")
-        b = input("Выберите действие: ")
+        b = input("Р’С‹Р±РµСЂРёС‚Рµ РґРµР№СЃС‚РІРёРµ: ")
         if b == "2":
 
             list_files = subprocess.run(["apt-get" , "install" , "irssi"])
@@ -418,7 +418,7 @@ while True:
             list_files = subprocess.run(["irssi"])
 
     else:
-        print("Задана неверная функция | Wrong fuction has been setup")
+        print("Р—Р°РґР°РЅР° РЅРµРІРµСЂРЅР°СЏ С„СѓРЅРєС†РёСЏ | Wrong fuction has been setup")
 
     time.sleep(7)
 
